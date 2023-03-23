1e2c3c556de7b2e7578eb784ede5e3084d3856fa
Vinícius dos Santos Alves <viniciuus.santos@aluno.uece.br>
2023-03-23 19:19:47 -0300

    Update ListaSequencial.h

--- ListaSequencial.h
+++ ListaSequencial.h
@@ -7,16 +7,18 @@ typedef struct aluno{
     int matricula;
     float notas[3];
 
-}Aluno; 
+}Aluno;
 
 typedef struct lista Lista;
 
 void inicializa_lista(Lista* li);
 int insere_lista_inicio(Lista* li, Aluno al);
 int insere_lista_fim(Lista* li, Aluno al);
+int insere_lista_ordenada(Lista* li, Aluno al);
 int remove_lista_inicio(Lista* li);
 int remove_lista_fim(Lista* li);
-Aluno busca_lista_pos(Lista* li, int pos);
+int remove_lista_otimizado(Lista* li, int mat);
+void busca_lista_pos(Lista* li, int pos);
 void busca_lista_mat(Lista* li, int matricula);
 void imprime_lista(Lista* li);
 
